6012231ab3a560e4a9decd93bd71eefb4a814b77
Alejandro Roa <ingjesusroa@gmail.com>
2017-06-14 14:57:37 -0700

    add method create in AuthenticationController: get request, send data to method AuthenticateUser and response json

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,5 @@
 class AuthenticationController < ApplicationController
+  before_action :authenticate_request, only: [:authenticate, :create]
   skip_before_action :authenticate_request
 
   def authenticate
@@ -10,4 +11,18 @@ class AuthenticationController < ApplicationController
       render json: { error: command.errors }, status: :unauthorized
     end
   end
+
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    render json: {user: auth_token }
+  end
+
+  # we used strong parameters for the validation of params
+  private
+
+  def user_params
+    params.permit(:name,:email,:password, :password_confirmation)
+  end
+
 end
