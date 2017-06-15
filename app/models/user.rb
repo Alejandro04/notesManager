84237f04498ac6a4dbf9c11fbc48335d0d709fa8
Alejandro Roa <ingjesusroa@gmail.com>
2017-06-14 17:33:48 -0700

    save a categoy - method create in CategoriesController

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :password_digest, presence: true
+  has_many :category
 end
---
+++ spec/factories/categories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :category do
+    name "MyString"
+    description "MyText"
+    user nil
+  end
+end
